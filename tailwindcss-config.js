ddbf7130ae6aa961366c365b3d4fa2b74c0e7183
LewinsHp <lewinsanhamonanganpanjaitan@gmail.com>
2023-03-09 09:04:27 +0700

    cracojs

--- tailwindcss-config.js
+++ tailwindcss-config.js
@@ -1,8 +1,46 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
-  content: [],
+  purge: ["./src/**/*.{js,jsx,ts,tsx}", ".public/index.html"],
+  darkMode: false,
   theme: {
+    extend: {
+      spacing: {
+        88: "22rem",
+        92: "23rem"
+      },
+      colors: {
+        purple: {
+          DEFAULT: "#471F3C",
+        },
+        yellow: {
+          DEFAULT: "#DC9E25",
+          light: "#E5B350",
+        },
+        orange: {
+          DEFAULT: "#DA6D0D",
+          light: "#E5B350",
+        },
+        pink: {
+          DEFAULT: "#F4D1A7",
+          light: "#F4EEE0",
+        },
+        gray: {
+          DEFAULT: "#E5DDC5",
+        },
+      },
+      maxWidth: {
+        "8xl": "88rem",
+      },
+    },
+    screens: {
+      xs: "576px",
+      ...defaultTheme.screens,
+    },
+  },
+  variants: {
     extend: {},
   },
   plugins: [],
-}
+};
